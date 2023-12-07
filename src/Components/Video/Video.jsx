ddc655f078398c6e199edea92075b6edc9a06b2d
DarkCode662432 <ff9371668@gmail.com>
2023-12-07 09:41:36 +0200

    fix hero section responsive

--- src/Components/Video/Video.jsx
+++ src/Components/Video/Video.jsx
@@ -5,13 +5,13 @@ import PropTypes from 'prop-types';
 // Video component
 const Video = ({ src }) => (
   // Video container with rounded border
-  <div className='w-full bg-white rounded-2xl p-[22px] pt-[14px] flex flex-col shadow-custom-light'>
+  <div className='w-full bg-white rounded-2xl p-[22px] max-md:p-4 pt-[14px] max-md:pt-3 flex flex-col shadow-custom-light'>
 
     {/* Decorative elements */}
-    <div className="flex justify-start items-center gap-2 pb-[14px]">
-      <span className='w-4 aspect-square rounded-full bg-red-1'></span>
-      <span className='w-4 aspect-square rounded-full bg-[#F4C245]'></span>
-      <span className='w-4 aspect-square rounded-full bg-[#53B477]'></span>
+    <div className="flex justify-start items-center gap-2 pb-[14px] max-md:pb-3">
+      <span className='w-4 max-md:w-3 aspect-square rounded-full bg-red-1'></span>
+      <span className='w-4 max-md:w-3 aspect-square rounded-full bg-[#F4C245]'></span>
+      <span className='w-4 max-md:w-3 aspect-square rounded-full bg-[#53B477]'></span>
     </div>
 
     {/* Video player */}
