53e56abb57d17370301fc4911d5660946b11f1a0
DarkCode662432 <ff9371668@gmail.com>
2023-12-07 09:51:54 +0200

    Gallery section responsive

--- src/Components/Gallery/Percent.jsx
+++ src/Components/Gallery/Percent.jsx
@@ -5,13 +5,13 @@ import PropTypes from 'prop-types';
 // Percent component
 const Percent = ({ percent, text }) => {
   return (
-    <div className='flex flex-col gap-4 max-w-[200px]'>
+    <div className='flex flex-col gap-4 max-w-[200px] max-md:max-w-[140px]'>
       {/* Percent Value */}
-      <span className='text-red-1 text-8xl font-black py-4'>
+      <span className='text-red-1 text-8xl max-md:text-7xl font-black py-4'>
         {percent}%
       </span>
       {/* Accompanying Text */}
-      <span className='font-black text-[16px]'>
+      <span className='font-black text-[16px] max-md:text-sm'>
         {text}
       </span>
     </div>
