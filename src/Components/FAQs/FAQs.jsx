529f95662128664cff4c2129fc4792152e024d62
hamdysaad20 <hamdysaad.biz@gmail.com>
2023-12-16 11:54:53 +0200

    Refactor FAQs and Pricing components

--- src/Components/FAQs/FAQs.jsx
+++ src/Components/FAQs/FAQs.jsx
@@ -1,32 +1,51 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Accordion, AccordionItem } from "@nextui-org/react";
+import {Accordion, AccordionItem} from "@nextui-org/react";
 
-/**
- * FAQs component displays a list of frequently asked questions. 
- * Each question is wrapped within an AccordionItem component, which 
- * handles the expand/collapse functionality.
- *
- * This approach adheres to WCAG guideline 1.3.1 (Info and Relationships) because 
- * the information, structure, and relationships conveyed through presentation can be 
- * programmatically determined or are available in the text.
- *
- * @param {Object[]} questions Array of question objects
- * @returns {JSX.Element} Rendered FAQs component
- */
+
 function FAQs({ questions }) {
   return (
-    <div className="flex gap-4 m-0  pb-12 justify-center" radius="md">
-      <div className='w-[700px]  '>
-      <Accordion selectionMode="multiple">
+    <div>
+      <div className='w-full justify-center flex pt-0 pb-12 '>
+        <div className='flex flex-col gap-6 text-[#121212] text-center  box-border px-0 lg:px-16 max-lg:px-2 max-md:px-0 max-w-[890px]'>
+          <div
+            style={{
+              fontFamily:
+                'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
+              fontWeight: "bold",
+              lineHeight: "120%",
+            }}
+            className='text-4xl md:text-7xl font-bold relative leading-120%'>
+            <span>Frequently Asked Questions</span>
+          </div>
+          <p className='text-[24px] max-lg:text-[20px] max-mg:text-[18px] box-border px-[76px] max-lg:px-28 max-md:px-0 leading-[150%] text-gray-700'>
+            Here are some of the most common questions we get asked. If you have
+            other questions, please{" "}
+            <a
+            className='text-[#3092DB] hover:text-[#3092DB] transition underline underline-offset-2 duration-500 ease-in-out'
+              href='mailto:info@neferdata.com ' 
+              >Contact us</a>
+                .
+            
+          </p>
+        </div>
+      </div>
+      
+      <div className="flex gap-4 m-0  pb-12 justify-center" radius="md">
+      <div className='w-full max-w-[1000px] px-4  '>
+      <Accordion  selectionMode="multiple">
         {questions.map((question, idx) => (
-          <AccordionItem key={idx} aria-label={question.title} title={question.title}>
+          <AccordionItem key={idx} aria-label={question.title}
+          
+          
+          title={question.title}>
             {question.discription}
           </AccordionItem>
         ))}
       </Accordion>
       </div>
     
+    </div>
     </div>
   );
 }
--- src/Pages/Pricing/Pricing.jsx
+++ src/Pages/Pricing/Pricing.jsx
@@ -1,13 +1,13 @@
-import { MdOutlineDone } from 'react-icons/md';
-import PricingCard from '../../Components/Card/PricingCard';
+import { MdOutlineDone } from "react-icons/md";
+import PricingCard from "../../Components/Card/PricingCard";
 
 function Pricing() {
   const pricingCardsData = [
     {
-      className: '',
-      title: 'Standerd',
-      price: 'Free',
-      renew: 'Always',
+      className: "",
+      title: "Standerd",
+      price: "Free",
+      renew: "Always",
       features: [
         "AI-powered email filter",
         "Snack-sized company insights",
@@ -17,13 +17,13 @@ function Pricing() {
         "No adds or paid placement",
         "Hustle-free hosting in global instance",
       ],
-      buttonText: 'Create Account'
+      buttonText: "Create Account",
     },
     {
-      className: '',
-      title: 'Self-hosted',
-      price: 'Free',
-      renew: 'Always',
+      className: "",
+      title: "Self-hosted",
+      price: "Free",
+      renew: "Always",
       features: [
         "All features of Standard",
         "Full data protection of your environment",
@@ -33,14 +33,14 @@ function Pricing() {
         "Audit log & reports of extracted insights",
         "Access to upgrades & new features at your timeline",
       ],
-      buttonText: 'See Documentation'
+      buttonText: "See Documentation",
     },
     // Enterprise pricing data
     {
-      className: '!bg-sky-100',
-      title: 'Enterprise',
-      price: '$ 5.99',
-      renew: 'per user per month',
+      className: "!bg-sky-100",
+      title: "Enterprise",
+      price: "$ 5.99",
+      renew: "per user per month",
       features: [
         "All features of Standard",
         "Full data protection & privacy",
@@ -50,36 +50,38 @@ function Pricing() {
         "Audit log & reports of extracted insights",
         "Immediate access to latest features",
       ],
-      buttonText: 'Purchase Now'
-    }
+      buttonText: "Purchase Now",
+    },
   ];
 
   return (
     <div className='bg-gradient-to-b from-[#3092DB] to-[#FBFCFF]'>
-   <div className='w-full justify-center flex pt-32 '>
-   <div className="flex flex-col gap-6 text-white text-center  box-border px-0 lg:px-16 max-lg:px-2 max-md:px-0 max-w-[890px]">
-    <h1
-    style={{fontFamily: 'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif', fontWeight: 'bold', lineHeight: '120%'}}
-    className="text-4xl md:text-7xl font-bold leading-120%">
-    Neferdata Pricing Plans 
-    </h1>
-    <p className="text-[24px] max-lg:text-[20px] max-mg:text-[18px] box-border px-[76px] max-lg:px-28 max-md:px-0 leading-[150%] text-gray-200">
- 
-   Standard, Self-hosted, and Enterprise. Upgrade anytime for advanced features and capabilities.
-  </p>
-  </div>
-   
-   </div>
-    <div className="w-full bg[#FBFCFF] py-20 flex flex-wrap box-border lg:flex-nowrap justify-center items-center gap-3 lg:gap-9">
-     
-      {pricingCardsData.map((cardData, idx) => (
-        // Using unique index as key for the mapped elements. And spread operator to pass props.
-        <PricingCard {...cardData} key={idx} />
-      ))}
+      <div className='w-full justify-center flex pt-32 '>
+        <div className='flex flex-col gap-6 text-white text-center  box-border px-0 lg:px-16 max-lg:px-2 max-md:px-0 max-w-[890px]'>
+          <h1
+            style={{
+              fontFamily:
+                'ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif',
+              fontWeight: "bold",
+              lineHeight: "120%",
+            }}
+            className='text-4xl md:text-7xl font-bold leading-120%'>
+            Neferdata Pricing Plans
+          </h1>
+          <p className='text-[24px] max-lg:text-[20px] max-mg:text-[18px] box-border px-[76px] max-lg:px-28 max-md:px-0 leading-[150%] text-gray-200'>
+            Standard, Self-hosted, and Enterprise. Upgrade anytime for advanced
+            features and capabilities.
+          </p>
+        </div>
+      </div>
+      <div className='w-full bg[#FBFCFF] py-20 flex flex-wrap box-border lg:flex-nowrap justify-center items-center gap-3 lg:gap-9'>
+        {pricingCardsData.map((cardData, idx) => (
+          // Using unique index as key for the mapped elements. And spread operator to pass props.
+          <PricingCard {...cardData} key={idx} />
+        ))}
+      </div>
     </div>
- 
-    </div> );
+  );
 }
 
 export default Pricing;
-
