be2185e9e91b0c3e0838ea8136b0e2d01dfa8333
hamdysaad20 <hamdysaad.biz@gmail.com>
2023-12-16 15:00:12 +0200

    Refactor NotFound component layout and add Lottie animation

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import Lottie from "lottie-react";
+import groovyWalkAnimation from "../Lottie/notfound.json";
 
 
 
 function NotFound() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
-            <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-            <p className="text-lg">The requested page could not be found.</p>
+       <div className="w-full justify-center flex">
+         <div className="flex flex-col items-center justify-center h-screen lg:w-2/3 xl:w-1/2">
+            <div className='h-20 w-20 lg:h-40 lg:w-40'>
+                <Lottie animationData={groovyWalkAnimation} loop={true} />
+            </div>
+            <h1 className="text-4xl text-center font-bold mb-4">404 - Page Not Found</h1>
+            <p className="text-lg text-center">The requested page could not be found.</p>
         </div>
+       </div>
     );  
 }
 
