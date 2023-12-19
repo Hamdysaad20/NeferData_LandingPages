92c4fc444943f8dbd6ac77e7675b60802b174db4
hamdysaad20 <hamdysaad.biz@gmail.com>
2023-12-19 20:36:53 +0200

    Update styles in Features and Testimonials components

--- src/Components/Features/Features.jsx
+++ src/Components/Features/Features.jsx
@@ -26,7 +26,7 @@ const Features = ({ tabs }) => {
       }}
       className='w-full overflow-hidden pt-32  '>
       <div className='w-full flex justify-center'>
-        <div className='bg-[#3092DB] w-54 p-2 shadow-2xl shadow-blue-700 rounded-3xl h-54'>
+        <div className='bg-[#3092DB] w-54 p-2 shadow-2xl shadow-blue-700 rounded-[2rem] h-54'>
           <UseAnimations
             animation={loading2}
             speed={0.5}
@@ -45,57 +45,55 @@ const Features = ({ tabs }) => {
           }}
           className='text-4xl md:text-7xl text-[#121212] max-w-[1000px] text-center font-bold relative leading-120%'>
           <div className='flex flex-col md:flex-row place-items-center'>
-            <span>
-              Neferdata is a suite of{" "}
-              <span className='underline decoration-sky-500'>
-                AI-powered tools
-              </span>{" "}
-              to help grow your business
-            </span>
+          <span>Neferdata: <span className='underline decoration-sky-500'>AI tools </span>for  <span className='underline decoration-sky-500'>business growth</span> .</span>
+
+
           </div>
         </div>
       </div>
       <section className='flex flex-col max-w-[1500px] w-[90%]  items-center justify-center m-auto gap-2 mb-48'>
         {/* Tab Generator */}
-        <div className='flex gap-2  rounded-3xl  overflow-hidden overflow-x-auto scroll-ml-6 md:justify-center  w-full px-4'>
+        <div className='flex gap-2 pb-6  rounded-3xl  overflow-hidden overflow-x-auto scroll-ml-6 md:justify-center  w-full px-4'>
           {tabs.map((tab, index) => (
             <button
               key={index}
               // If activeTab matches index, then set higher opacity
-              className={`flex select-none flex-col  hover:opacity-75 duration-500 active:scale-95 justify-start text-center items-center  rounded-3xl p-4  w-48  gap-3 font-semibold transition-all text-blue-1 ${
+              className={`flex select-none flex-col  hover:opacity-75 duration-500 active:scale-95 justify-start text-center items-center  rounded-3xl p-2  gap-3 font-semibold transition-all text-blue-1 ${
                 activeTab === index
-                  ? "opacity-100 hover:opacity-100"
+                  ? "opacity-100 shadow-sky-500/40 shadow-inner   hover:opacity-100"
                   : "opacity-50 hover:opacity-80"
               }`}
               onClick={() => handleTabSwitch(index)}>
-              <div className='h-14 aspect-[4/3] flex justify-center items-center rounded-2xl border-2 border-black text-black'>
+              <div className='h-12   w-full  flex justify-center gap-2 px-4 items-center  rounded-2xl border-2 border-black text-black'>
                 <tab.Icon />
+                <div className='leading-[16px] font-medium text-[0.875rem] text-black'>{tab.title}</div>
+
               </div>
-              <div className='leading-[14px] text-black'>{tab.title}</div>
             </button>
           ))}
+             
         </div>
         {/* Active Feature Data Display */}
-        <div className='flex flex-col h-[700px] bg-[#3092DB] backdrop-blur-2xl w-full rounded-2xl p-4 divide-y-2'>
+        <div className='flex flex-col h-[700px] bg-[#3092DB] backdrop-blur-2xl w-full rounded-2xl p-4 divide-white/20 divide-y-2'>
           <div className='flex overflow-hidden overflow-x-auto relative gap-4 pb-4'>
             {tabs[activeTab].features.map((feature, index) => (
               <button
                 key={index}
-                className={`grid ease-linear border-2 active:scale-95 duration-500 select-none place-content-center grid-cols-6 box-content relative h-12 w-54 text-center items-center rounded-2xl p-1 gap-3 font-semibold transition-all text-black ${
+                className={`grid place-items-center  select-none flex-col  hover:opacity-75 duration-500 active:scale-95 justify-start text-center items-center  rounded-2xl p-2 h-12   font-bold transition-all text-black ${
                   activeFeature === index
-                    ? "opacity-100 hover:opacity-100 bg-gray-200 border border-black"
+                    ? "opacity-100 hover:opacity-100 bg-gray-200 "
                     : "opacity-50 border-white/20 hover:opacity-80"
                 }`}
                 onClick={() => handleFeatureSwitch(index)}
                 style={{ flex: "none" }}>
-                <div className='flex justify-center items-center rounded-lg'>
-                  <feature.Icon />
-                </div>
-                <div
+                <div className='flex gap-2 px-2 justify-center items-center rounded-2xl'>
+                  <feature.Icon /> <div
                   className='grid col-span-5 place-items-center text-sm break-keep box-content '
-                  style={{ width: "calc(100% - 15px)" }}>
+                 >
                   {feature.title}
                 </div>
+                </div>
+               
               </button>
             ))}
           </div>
--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -7,7 +7,7 @@ const Testimonials = () => {
     <div
    
     className='w-full flex justify-center mb-0 p-2'>
-      <div  className='w-full bg-[#3092DB] h-[800px] md:h-[700px] rounded-3xl max-w-[1500px] '>
+      <div  className='w-full bg-[#3092DB] h-[900px] md:h-[700px] rounded-3xl max-w-[1500px] '>
         <div className='w-full justify-center flex pt-12'>
           <div className='flex flex-col gap-6 justify-center text-center box-border px-0 lg:px-16 max-lg:px-2 max-md:px-0 max-w-[890px]'>
             <div
@@ -22,7 +22,7 @@ const Testimonials = () => {
                 <span>What our customers are saying about us</span>
                 
               </div>
-              <div className='absolute h-20 w-20 right-5 md:right-20 -bottom-10 md:-bottom-2'>
+              <div className='absolute   h-20 w-20 right-5 md:right-20 -top-10'>
                 <Sparkels />
               </div>
             </div>
