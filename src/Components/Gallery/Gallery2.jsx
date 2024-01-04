848fa4b9011bacfde116fd7d1a05eb0e8f2b1125
hamdysaad20 <hamdysaad.biz@gmail.com>
2024-01-04 18:24:51 +0200

    fix

--- src/Components/Gallery/Gallery2.jsx
+++ src/Components/Gallery/Gallery2.jsx
@@ -8,12 +8,12 @@ export default function Gallery2() {
   const PERCENT_STATS = [
     {
       text: "55% of company apps are NOT used on a regular basis",
-      color: "text-[#FFFF00]",
+      color: "text-white",
       
     },
     {
       text: "70% of digital projects fail to deliver the expected biz value",
-      color: "text-black",
+      color: "text-white",
     },
     {
       text: "56% of SaaS providers fail for internal, non-product reasons",
@@ -33,14 +33,16 @@ export default function Gallery2() {
   }, []);
 
   return (
-    <section className="w-[90%] max-md:w-full max-w-[1200px] flex justify-center flex-col items-center mx-auto py-32 px-16 max-lg:px-8 max-md:px-6 gap-12  box-border">
-      <div className='bg-[#3092DB] w-54 p-2 shadow-2xl shadow-blue-700 rounded-3xl h-54'>
+    <section className="w-[90%] max-md:w-full max-w-[1400px] flex justify-center flex-col items-center mx-auto py-32 px-16 max-lg:px-8 max-md:px-6 gap-12  box-border">
+      <div className='bg-[#3092DB] p-2 shadow-2xl shadow-blue-700 rounded-3xl h-54'>
         <UseAnimations animation={infinity} size={100} strokeColor='white' />
       </div>
       <h2 className="font-black text-8xl  md:text-7xl max-md:text-sm text-center">
         Finding the right tool for your business is critical
       </h2>
-      <div className="h-[400px] relative sm:h-[700px] bg-[#3092DB] text-black  overflow-hidden rounded-2xl p-2 py-12 pt-32 w-full max-w-[1400px] flex justify-center items-center gap-12  box-border">
+
+      
+      <div className="h-[400px] relative sm:h-[700px] bg-[#3092DB] text-black  overflow-hidden rounded-2xl p-2 py-12 pt-32 w-full  flex justify-center items-center gap-12  box-border">
         <div className='absolute top-0 flex justify-center flex-col text-center  pt-4 left-0  w-full'>
 <div
  className='flex justify-center items-center gap-4'>
