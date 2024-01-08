2109ffa51ec519470d95f0b5f33abae095a30df2
hamdysaad20 <hamdysaad.biz@gmail.com>
2024-01-08 16:29:02 +0200

    Refactor FeaturesGrid component

---
+++ src/Components/FeaturesGrid/BGasset.jsx
@@ -0,0 +1,114 @@
+function BGasset() {
+  return (
+    <>
+      <svg
+        fill='none'
+        height={537}
+        viewBox='0 0 910 537'
+        width={910}
+        xmlns='http://www.w3.org/2000/svg'
+        className='problem-background'>
+        <path
+          d='M909.5 367V359M909.5 259V251M909.5 151V143'
+          stroke='black' // BEGIN: Change path color to black
+          strokeLinecap='round'
+          strokeOpacity='0.1'
+        />
+        <path
+          d='M253 1L901.5 1C905.918 1 909.5 4.58172 909.5 9V43'
+          stroke='black' // BEGIN: Change path color to black
+          strokeLinecap='round'
+          strokeOpacity='0.1'
+        />
+        <path
+          d='M149.5 31V23M149.5 235V207M149.5 311V307M149.5 411V407'
+          stroke='black' // BEGIN: Change path color to black
+          strokeLinecap='round'
+          strokeOpacity='0.1'
+        />
+        <path
+          d='M33 49H13C6.37258 49 1 54.3726 1 61V399C1 405.627 6.37258 411 13 411H33'
+          stroke='black' // BEGIN: Change path color to black
+          strokeLinecap='square'
+          strokeOpacity='0.1'
+        />
+        <path
+          d='M109 1H49C40.1634 1 33 8.16345 33 17V443C33 451.837 40.1634 459 49 459H109'
+          stroke='black' // BEGIN: Change path color to black
+          strokeLinecap='round'
+          strokeOpacity='0.1'
+        />
+        <path
+          d='M149.5 505V528.5C149.5 532.918 153.082 536.5 157.5 536.5H901.5C905.918 536.5 909.5 532.918 909.5 528.5V465'
+          stroke='black' // BEGIN: Change path color to black
+          strokeLinecap='round'
+          strokeOpacity='0.1'
+        />
+        <defs>
+          <linearGradient
+            gradientUnits='userSpaceOnUse'
+            id='paint0_linear_280_310'
+            x1='676.398'
+            x2='676.398'
+            y1={1}
+            y2='538.5'>
+            <stop stopColor='#3B2F57' />
+            <stop offset={1} stopColor='#7D643E' />
+          </linearGradient>
+          <linearGradient
+            gradientUnits='userSpaceOnUse'
+            id='paint1_linear_280_310'
+            x1='676.398'
+            x2='676.398'
+            y1={1}
+            y2='538.5'>
+            <stop stopColor='#3B2F57' />
+            <stop offset={1} stopColor='#7D643E' />
+          </linearGradient>
+          <linearGradient
+            gradientUnits='userSpaceOnUse'
+            id='paint2_linear_280_310'
+            x1='676.398'
+            x2='676.398'
+            y1='0.999999'
+            y2='538.5'>
+            <stop stopColor='#3B2F57' />
+            <stop offset={1} stopColor='#7D643E' />
+          </linearGradient>
+          <linearGradient
+            gradientUnits='userSpaceOnUse'
+            id='paint3_linear_280_310'
+            x1='676.398'
+            x2='676.398'
+            y1={1}
+            y2='538.5'>
+            <stop stopColor='#3B2F57' />
+            <stop offset={1} stopColor='#7D643E' />
+          </linearGradient>
+          <linearGradient
+            gradientUnits='userSpaceOnUse'
+            id='paint4_linear_280_310'
+            x1='676.398'
+            x2='676.398'
+            y1={1}
+            y2='538.5'>
+            <stop stopColor='#3B2F57' />
+            <stop offset={1} stopColor='#7D643E' />
+          </linearGradient>
+          <linearGradient
+            gradientUnits='userSpaceOnUse'
+            id='paint5_linear_280_310'
+            x1='676.398'
+            x2='676.398'
+            y1='-0.999975'
+            y2='536.5'>
+            <stop stopColor='#3B2F57' />
+            <stop offset={1} stopColor='#7D643E' />
+          </linearGradient>
+        </defs>
+      </svg>
+    </>
+  );
+}
+
+export default BGasset;
--- src/Components/FeaturesGrid/FeaturesGrid.jsx
+++ src/Components/FeaturesGrid/FeaturesGrid.jsx
@@ -1,157 +1,85 @@
+
+import Kubernetes from './Kubernetes';
+import BGasset from './BGasset';
+import Smallasste from './Smallasste';
 function FeaturesGrid() {
+ 
+
   return (
     <div className='flex justify-center  '>
+      
       <section className='container  flex justify-center relative rounded-3xl'>
+        
+      <div className='section-icon  flex justify-center'>
+      <Smallasste/>
+
+           </div>
         <div className=' w-full'>
-          <div className='section-icon py-6 flex justify-center'>
-            <svg
-              fill='none'
-              height={42}
-              viewBox='0 0 42 42'
-              width={42}
-              xmlns='http://www.w3.org/2000/svg'
-              q:key='tw_0'>
-              <path
-                d='M20.2209 12.3954C16.8295 11.6332 14.2072 10.2621 12.7309 7.75698C9.6182 2.47508 16.5452 -1.88836 19.6579 3.39353C21.1342 5.89871 21.1169 8.93231 20.2209 12.3954ZM20.2209 12.3954C28.7442 14.3023 35.3175 17.7693 39.0282 24.0659C46.832 37.3079 29.4783 48.2394 21.6745 34.9973C17.8906 28.5765 18.1494 20.2059 20.2209 12.3954ZM20.2209 12.3954C21.1282 10.5166 22.3138 8.96362 24.0549 7.8669C27.7257 5.55454 29.5395 8.63227 25.8686 10.9446C24.1276 12.0414 22.2509 12.4215 20.2209 12.3954ZM20.2209 12.3954C17.9461 17.1159 14.9553 21.0127 10.5792 23.7693C1.37611 29.5665 -3.16777 21.8561 6.03536 16.0589C10.4978 13.2479 15.6491 12.3097 20.2209 12.3954Z'
-                stroke='#454D76'
-                strokeLinejoin='round'
-                strokeOpacity='0.16'
-              />
-            </svg>
-          </div>
+         
           <div className='flex justify-start     w-full'>
-            <svg
-              fill='none'
-              height={537}
-              viewBox='0 0 910 537'
-              width={910}
-              xmlns='http://www.w3.org/2000/svg'
-              className='problem-background'>
-              <path
-                d='M909.5 367V359M909.5 259V251M909.5 151V143'
-                stroke='url(#paint0_linear_280_310)'
-                strokeLinecap='round'
-                strokeOpacity='0.1'
-              />
-              <path
-                d='M253 1L901.5 1C905.918 1 909.5 4.58172 909.5 9V43'
-                stroke='url(#paint1_linear_280_310)'
-                strokeLinecap='round'
-                strokeOpacity='0.1'
-              />
-              <path
-                d='M149.5 31V23M149.5 235V207M149.5 311V307M149.5 411V407'
-                stroke='url(#paint2_linear_280_310)'
-                strokeLinecap='round'
-                strokeOpacity='0.1'
-              />
-              <path
-                d='M33 49H13C6.37258 49 1 54.3726 1 61V399C1 405.627 6.37258 411 13 411H33'
-                stroke='url(#paint3_linear_280_310)'
-                strokeLinecap='square'
-                strokeOpacity='0.1'
-              />
-              <path
-                d='M109 1H49C40.1634 1 33 8.16345 33 17V443C33 451.837 40.1634 459 49 459H109'
-                stroke='url(#paint4_linear_280_310)'
-                strokeLinecap='round'
-                strokeOpacity='0.1'
-              />
-              <path
-                d='M149.5 505V528.5C149.5 532.918 153.082 536.5 157.5 536.5H901.5C905.918 536.5 909.5 532.918 909.5 528.5V465'
-                stroke='url(#paint5_linear_280_310)'
-                strokeLinecap='round'
-                strokeOpacity='0.1'
-              />
-              <defs>
-                <linearGradient
-                  gradientUnits='userSpaceOnUse'
-                  id='paint0_linear_280_310'
-                  x1='676.398'
-                  x2='676.398'
-                  y1={1}
-                  y2='538.5'>
-                  <stop stopColor='#3B2F57' />
-                  <stop offset={1} stopColor='#7D643E' />
-                </linearGradient>
-                <linearGradient
-                  gradientUnits='userSpaceOnUse'
-                  id='paint1_linear_280_310'
-                  x1='676.398'
-                  x2='676.398'
-                  y1={1}
-                  y2='538.5'>
-                  <stop stopColor='#3B2F57' />
-                  <stop offset={1} stopColor='#7D643E' />
-                </linearGradient>
-                <linearGradient
-                  gradientUnits='userSpaceOnUse'
-                  id='paint2_linear_280_310'
-                  x1='676.398'
-                  x2='676.398'
-                  y1='0.999999'
-                  y2='538.5'>
-                  <stop stopColor='#3B2F57' />
-                  <stop offset={1} stopColor='#7D643E' />
-                </linearGradient>
-                <linearGradient
-                  gradientUnits='userSpaceOnUse'
-                  id='paint3_linear_280_310'
-                  x1='676.398'
-                  x2='676.398'
-                  y1={1}
-                  y2='538.5'>
-                  <stop stopColor='#3B2F57' />
-                  <stop offset={1} stopColor='#7D643E' />
-                </linearGradient>
-                <linearGradient
-                  gradientUnits='userSpaceOnUse'
-                  id='paint4_linear_280_310'
-                  x1='676.398'
-                  x2='676.398'
-                  y1={1}
-                  y2='538.5'>
-                  <stop stopColor='#3B2F57' />
-                  <stop offset={1} stopColor='#7D643E' />
-                </linearGradient>
-                <linearGradient
-                  gradientUnits='userSpaceOnUse'
-                  id='paint5_linear_280_310'
-                  x1='676.398'
-                  x2='676.398'
-                  y1='-0.999975'
-                  y2='536.5'>
-                  <stop stopColor='#3B2F57' />
-                  <stop offset={1} stopColor='#7D643E' />
-                </linearGradient>
-              </defs>
-            </svg>
+<BGasset/>
           </div>
         </div>
 
-      <div className="div absolute h-full w-full">
-      <div className="parent-div h-full w-full flex flex-col md:flex-row">
-        {/* Left Div */}
-        <div className="left-div flex w-full h-full flex-col">
-          {/* Top Div */}
-          <div className="top-div h-full w-full bg-blue-500 p-4">
-            {/* Content for Top Div */}
-            <p>Top Div Content</p>
-          </div>
-          {/* Bottom Div */}
-          <div className="bottom-div h-full w-full bg-green-500 p-4 ">
-            {/* Content for Bottom Div */}
-            <p>Bottom Div Content</p>
-          </div>
-        </div>
+        <div className='div absolute pt-10 h-full w-full'>
+          <div className='parent-div h-full w-full flex flex-col md:flex-row'>
+            {/* Left Div */}
+            <div className='left-div flex w-full h-full flex-col'>
+              {/* Top Div */}
+              <div className='top-div h-full w-full  p-2'>
+                {/* Content for Top Div */}
+                <div className='bg-[#3092DB] p-4 rounded-xl w-full h-full'>
+                  <h1 className='text-3xl font-bold text-white'>
+                    Flexible Deployment Options
+                  </h1>
+                  <p className=' text-white text-sm font-medium mt-2 md:w-9/12'>
+                    Neferdata provides deployment options: free Global Instance,
+                    Managed Service, and Self-Hosted control.{" "}
+                  </p>
+                </div>
+              </div>
+              {/* Bottom Div */}
+              <div className='bottom-div h-full w-full  p-2 '>
+                {/* Content for Bottom Div */}
+                <div className='bg-[#3092DB] p-4 rounded-xl w-full h-full'>
+                  <h1 className='text-3xl font-bold text-white'>
+                    Automated Setup &amp; Configuration
+                  </h1>
+                  <p className=' text-white text-sm font-medium mt-2 md:w-9/12'>
+                    Automate Neferdata setup using Terraform and Helm, with
+                    support via mail and phone.{" "}
+                  </p>
+                </div>
+              </div>
+            </div>
 
-        {/* Right Div */}
-        <div className="right-div w-full h-full bg-red-500 p-4 flex-grow">
-          {/* Content for Right Div */}
-          <p>Right Div Content</p>
+            {/* Right Div */}
+            <div className='right-div w-full h-full  p-2 flex-grow'>
+              {/* Content for Right Div */}
+              <div      
+          
+              
+              id="CntainerKubernetesICONSVG" className={`bg-[#3092DB] overflow-hidden relative p-4 rounded-xl w-full h-full `}>
+                <h1 className='text-3xl font-bold text-white'>
+                  Set Up Your Kubernetes Cluster
+                </h1>
+                <p className=' text-white text-sm font-medium mt-2 md:w-9/12'>
+                  For Neferdata deployment, a scalable Kubernetes cluster is
+                  essential, adapting seamlessly to your business growth and
+                  changes.
+                </p>
+
+               <div
+                  
+               
+                  className='absolute bottom-0  right-0 flex justify-center items-center'
+                  style={{ width: "70%", height: "70%" }}>
+               <Kubernetes />
+               </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
       </section>
     </div>
   );
