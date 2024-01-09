4a886382f80449866fbe8f1c6019f3aae725282d
hamdysaad20 <hamdysaad.biz@gmail.com>
2024-01-09 15:01:33 +0200

    restructure

--- src/Components/Navbar/NavbarLink.jsx
+++ src/Components/Navbar/NavbarLink.jsx
@@ -1,20 +1,22 @@
-import PropTypes from 'prop-types';
-import { Link, useLocation } from 'react-router-dom';
+import PropTypes from "prop-types";
+import { Link, useLocation } from "react-router-dom";
 
-const NavbarLink = ({ links, main, hasDropDown }) => {
+const NavbarLink = ({ main }) => {
   const location = useLocation();
 
   return (
-    <div className='relative flex items-center justify-center cursor-pointer max-lg:py-5 h-6 max-lg:w-full text-center' role="menu">
+    <div
+      className='relative select-none flex items-center justify-center cursor-pointer max-lg:py-5 h-6 max-lg:w-full text-center'
+      role='menu'>
       {/* Display the main link */}
-      <Link 
+      <Link
         to={main.link}
         aria-label={main.title}
-        className={location.pathname === main.link ? ' text-sky-500 duration-700 ' : ''}
-      >
+        className={
+          location.pathname === main.link ? " text-sky-500 duration-700 " : ""
+        }>
         {main.title}
       </Link>
-     
     </div>
   );
 };
@@ -22,7 +24,7 @@ const NavbarLink = ({ links, main, hasDropDown }) => {
 NavbarLink.propTypes = {
   links: PropTypes.array.isRequired,
   main: PropTypes.object.isRequired,
-  hasDropDown: PropTypes.bool.isRequired
+  hasDropDown: PropTypes.bool.isRequired,
 };
 
 export default NavbarLink;
