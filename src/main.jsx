f5b14ecac251cee2b03aa80e3b7dda961ffa3d65
hamdysaad20 <hamdysaad.biz@gmail.com>
2024-01-16 17:42:17 +0200

    Refactor Navbar component and add Mission page

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,19 @@
-// Import React and required modules from react-router
 import ReactDOM from 'react-dom';
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
 } from "react-router-dom";
 
-// Import page components
 import LandingPage from './Pages/LandingPage/LandingPage';
 import Pricing from './Pages/Pricing/Pricing';
 import App from './Pages/App';
-import NotFound from './Pages/NotFound'; // Import the 404 page component
+import NotFound from './Pages/NotFound'; 
+import Mission from './Pages/MissionPage/Mission';
 
-// Import style sheets
 import './styles/index.css';
 import './styles/fonts.css';
-import Mission from './Pages/MissionPage/Mission';
+
 import ScrollToTop from './ScrolltoTop';
 
 
@@ -25,7 +22,6 @@ export function Application() {
     <Router>
       <ScrollToTop />
         <Routes>
-        
           <Route path="/" element={<App />}>
             <Route index element={<LandingPage />} />
             <Route path="/pricing" element={<Pricing />} />
